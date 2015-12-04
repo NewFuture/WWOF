08f7a10c5c721e74c2a686df3fda6296e652aad6
zhaowenjian <271118105@qq.com>
2015-12-04 20:30:54 +0800

    socket success
    调通socket 可下载远程文件

--- node/app.js
+++ node/app.js
@@ -1,25 +1,72 @@
-var app = require('http').createServer(handler)
+
+
+//原生ws：
+
+var http = require('http');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var download = require('./down.js')
+//var Worker = require('./lib/node-worker-master/lib/worker');
+
+
+var app = http.createServer().listen( 8888 );
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer( { server : app } );
+
+wss.on('connection', function( ws ) {
+    console.log('connection successful!');
+    ws.send('hello cleient');
+
+    ws.on('message', function( data, flags ) {
+
+        var jsonObj = JSON.parse(data);
+        console.log(jsonObj);
+
+        if( jsonObj.type == 'url' ){
+
+            download(jsonObj.url, function(file_name){
+                ws.send("Success download " + file_name);
+                var worker = new Worker('./download/' + file_name);
+            });          
+
+        }
+        
+        //do something here
+        
+    });
+    ws.on('close', function() {
+        console.log('stopping client');
+    });
+
+});
+
+
+
+
+
+/*var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
 app.listen(8888);
 
 function handler (req, res) {
-  
+
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
+	if (err) {
+	  res.writeHead(500);
+	  return res.end('Error loading index.html');
+	}
+	res.writeHead(200);
+	res.end(data);
   });
 }
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
-    console.log(data);
+	console.log(data);
   });
-});
+});*/
