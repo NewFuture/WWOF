df8b4ee1f362dd33048d1ffc6fefd3fd2ccffffe
NewFuture <new_future@msn.cn>
2016-10-05 21:36:54 +0800

    :gun: 使用submodule

--- node/README.md
+++
@@ -1,3 +0,0 @@
-#Offloading服务器目录
- 
- node代码根目录
--- web/test/log.js
+++
@@ -1,9 +0,0 @@
-//简单log
-console.log('log worker start!');
-var log = function(data) {
-	console.log('worker said log:' + data);
-	postMessage('LOG:' + data);
-};
-onmessage = function(ent) {
-	log(ent.data);
-};
